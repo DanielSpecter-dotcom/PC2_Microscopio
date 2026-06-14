8e581e535b804bd381c9399434447c632b7456b0
pilcovaleriia <pilcochuy.valeria@gmail.com>
2026-06-13 19:39:27 -0500

    notita

--- src/app/model/microscopio.ts
+++ src/app/model/microscopio.ts
@@ -9,3 +9,7 @@ export class Microscopio {
 
 //Copil, me dio mucho sueño solo de ver el pdf, dejé un .md con el paso a paso guiado de lo que hizo el profe en clase.
 //Confio, hijita
+//ya hice all the work (no se pq si pongo la palabra t-o-d-o me marca la linea vere radioactivo y m da miedoayuda xeso el ingles)
+// muere solo ojea que si funcione t-o-d-o bien y haces el word con las capturas y eso ALSO tuve q agregarle un metodo al backend ah
+//culpa del profe pq tenemos q utilizar en el registrar el listarid para q al registrar t redirija ahi y eso no lo hizo en el backend so nos odia pero ahi lo agregue t paso por wpp
+//go repo hoy mas tarde :3 muereporfavor
